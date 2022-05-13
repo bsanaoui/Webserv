c29e81bfa28a2e31633c6f4a5c8322db3c2f9ff4
brahim <brahim@gmail.com>
2022-05-13 14:16:57 +0100

    Create micro server

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Webserv
 This project is about writing my ow HTTP server.
 
+
+[//]: # (<< -------------------------- Setup Socket -------------------------------- >>)
+
 # Socket 
   - Create an endpoint for communication
   - **Endpoint** any device that connects to a computer network.
@@ -21,10 +24,34 @@ This project is about writing my ow HTTP server.
 
 * Return is a fd;
 
+* The original socket that was set up for listening is used only for accepting connections ,not for exchanging data
+
 ## 2- Identify (name) a socket: 
 * Assigning a transport address to the socket (a port number in IP networking)
 * In sockets, this operation is called **binding** an address and the bind system call is used for this.
 * Bind actually creates a **file** in the file system.
 * int bind(int socketfd, const struct sockaddr *addr, socklen_t addrlen):
     - assigns the address specified by **addr** to the **socket** referred to by the **file descriptor sockfd**
-    - address : 
+
+## 3- On the server, wait for incoming connection:
+* int listen(int sockfd, int backlog);
+    - Make the socket like a **passive socket** or acceptor socket is a type of socket that is used to to accept incoming connection requests using **accept**
+    - Backlog : defines the maximum length to which the queue of pending connections for sockfd may grow.
+* int accept(int socket, struct sockaddr *restrict address, socklen_t *restrict address_len);
+    - Socket : was set for accepting connections with listen.
+    - Address : gets filed in with the address of the client that is doing the connect(add, port..)
+    - Grabs(attrape) the first connection request on the queue of pending connections (set up in listen) and creates a new socket for that connection.
+    - Socket operations are **synchronous**, or **blocking**, and **accept** will block until a connection is present on the queue.
+
+
+## 4- Send and recieve msgs:
+* We finally have connected sockets between a client(when you visit IP address of your server from a web browser) and a server!
+* Communication is the easy part. The same **read** and **write** system calls that work on files also work on sockets.
+
+## 5- Close the socket:
+* with close(new_socket);
+  
+[//]: # (<< ------------------------------------------------------------------ >>)
+
+# New Field
+
--- test.hpp
+++ test.hpp
@@ -1,21 +1,64 @@
 #include <iostream>
-#include <sys/socket.h>
+#include <sys/socket.h> // socket
+#include <netinet/in.h> // sockaddr_in
+#include <unistd.h> //read, write
 
-#define SERVER_PORT 80
-#define MAXLINE     4096
-#define SA struct sockaddr
+#define SERVER_PORT 8080
+
 
 int test()
 {
-    std::cout << "Hello, From Server" << std::endl;
+    int                 server_fd, new_socket;
+    long                valread;
+    struct sockaddr_in  address;
+    int                 addrlen = sizeof(address);
 
-    int server_fd;
+    char                *msg = (char * )std::string("Hello, from server!").c_str();
 
-    // if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    // {
-    //     std::cerr << "cannot create socket";ß
-    //     return (1);
-    // }
+    // Creating socket file desctiptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        std::cerr << "In socket" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;           // AF_INET for TCP prototcol
+    address.sin_addr.s_addr = INADDR_ANY;   // INADDR_ANY : binds the socket to all available interfaces.
+    address.sin_port = htons(SERVER_PORT);  // htons : translates a short integer from host byte order to network byte order
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+
+    if(bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0 )
+    {
+        std::cerr << "In bind" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0)
+    {
+        std::cerr << "In listen" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        std::cout << "\n+++++++ Waiting for new connection ++++++++\n" << std::endl;
+        if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t*) &addrlen)) < 0)
+        {
+            std::cerr << "In accept" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+
+        char buffer[30000] = {0};
+        valread = read (new_socket, buffer, 30000);
+        if(valread < 0)
+            std::cout << "No bytes are there to read" << std::endl;
+
+        std::cout << buffer;
+        write(new_socket, msg, strlen(msg));
+        std::cout << "\n+++++++ Hello message sent ++++++++\n" << std::endl;
+        close(new_socket);
+    }
 
     return (0);
 }
